f7200dab874f88782a6b841873fdd1dd24329fa1
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-08-18 11:52:41 +0200

    loading articles moved from button to component onload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Users from './components/Users';
 import PostList from './components/PostList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Beginning</h1>
-      <Users />
+      <h1>Blog</h1>
       <PostList />
     </div>
   );
--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -3,25 +3,29 @@ import Post from './Post';
 
 const PostList = () => {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchPosts = () => {
+      setLoading(true);
+      fetch('http://localhost:8000/api/posts')
+        .then(res => res.json())
+        .then(data => setPosts(data))
+        .then(() => setLoading(false));
+    };
+    fetchPosts();
+  }, [])
 
   useEffect(() => {
     console.log('from useEffect hook', posts);
   }, [posts]);
 
-  const fetchPosts = () => {
-    fetch('http://localhost:8000/api/posts')
-      .then(res => res.json())
-      .then(data => setPosts(data));
-  };
-
   return (
     <div className="container">
       <h1>Recent posts</h1>
-      <p>Hello from PostList</p>
-      <button type="button" id="fetch" onClick={fetchPosts}>Fetch posts</button>
-      {posts
+      {!loading
         ? posts.map(post => {
-          return <Post key={post.id} title={post.data.title} body={post.data.body} author={post.data.author} date={post.data.date} /> 
+          return <Post key={post.id} title={post.data.title} body={post.data.body} author={post.data.author} date={post.data.date} />
         })
         : <h1>loading posts...</h1>}    
     </div>

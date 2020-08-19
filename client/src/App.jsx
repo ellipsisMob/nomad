a3c54a1d046566cb43befe6881fb60afd38639fd
marc <10080192+mhlgoossens@users.noreply.github.com>
2020-08-19 10:05:59 +0200

    introduce router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 import PostList from './components/PostList';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
       <h1>Blog</h1>
-      <PostList />
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/profile">profile</Link>
+          </li>
+          <li>
+            <Link to="/asdf">should show not found</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <PostList />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route render={() => 'Page not found'} />
+        </Switch>
+      </Router>
     </div>
   );
 }

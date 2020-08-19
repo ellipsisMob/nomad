9856abfe2dabc70a4e62a2cc4902ea803ecaa433
Sander Koopmans <42515367+SanderKoopmans@users.noreply.github.com>
2020-08-19 14:39:50 +0200

    merge final

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router,
+import {
+  BrowserRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
@@ -8,6 +9,7 @@ import Topbar from './components/Topbar';
 import Nav from './components/Nav';
 import PostList from './components/PostList';
 import Profile from './components/Profile';
+import UserList from './components/UserList';
 
 function App() {
   return (
@@ -16,14 +18,17 @@ function App() {
         <Topbar />
         <Nav />
         <Switch>
-      <Route exact path="/">
-        <PostList />
-      </Route>
-      <Route path="/profile">
-        <Profile />
-      </Route>
-      <Route render={() => 'Page not found'} />
-    </Switch>
+          <Route exact path="/">
+            <PostList />
+          </Route>
+          <Route path="/users/:id">
+            <Profile />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+          <Route render={() => 'Page not found'} />
+        </Switch>
       </Router>
     </div>
   );
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  Link,
-} from 'react-router-dom';
-import PostList from './PostList';
-import Profile from './Profile';
-import UserList from './UserList';
+import { Link } from 'react-router-dom';
 import './Nav.css';
 
 const Nav = () => (
@@ -13,31 +8,10 @@ const Nav = () => (
       <li>
         <Link to="/">Home</Link>
       </li>
-      {/* <li>
-        <Link to="/profile">profile</Link>
-      </li> */}
-      <li>
-        <Link to="/asdf">should show not found</Link>
-      </li>
       <li>
         <Link to="/users">Users</Link>
       </li>
     </ul>
-    <Switch>
-      <Route exact path="/">
-        <PostList />
-      </Route>
-      {/* <Route path="/profile">
-        <Profile />
-      </Route> */}
-      <Route path="/users/:id">
-        <Profile />
-      </Route>
-      <Route path="/users">
-        <UserList />
-      </Route>
-      <Route render={() => 'Page not found'} />
-    </Switch>
   </div>
 );
 

bd4f364d779ed7227c2bd0cf8f3f0dac7fa4e5b0
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-08-20 17:11:20 +0200

    Calender styled with margin

--- client/src/components/calendar/EventCalender.jsx
+++ client/src/components/calendar/EventCalender.jsx
@@ -4,19 +4,18 @@ import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import events from './__MOCKS__/events';
 
-// Setup the localizer by providing the moment (or globalize) Object
-// to the correct localizer.
-const localizer = momentLocalizer(moment) // or globalizeLocalizer
+const localizer = momentLocalizer(moment);
 
 const EventCalendar = props => {
   return (
     <div>
       <Calendar
+        className='eventCalender'
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
-        style={{ height: 570 }}
+        style={{ height: 570, margin: 20 }}
       />
     </div>
   )

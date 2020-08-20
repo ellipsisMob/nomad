69bfd2a3a78be36e8fd102f264e76255f3eda1b7
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-08-20 16:38:00 +0200

    calender working with mock data

--- client/src/components/calendar/EventCalender.jsx
+++ client/src/components/calendar/EventCalender.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import events from './__MOCKS__/events';
 
 // Setup the localizer by providing the moment (or globalize) Object
 // to the correct localizer.
-const localizer = Calendar.momentLocalizer(moment) // or globalizeLocalizer
+const localizer = momentLocalizer(moment) // or globalizeLocalizer
 
-const MyCalendar = props => {
+const EventCalendar = props => {
   return (
     <div>
       <Calendar
@@ -15,70 +16,10 @@ const MyCalendar = props => {
         events={events}
         startAccessor="start"
         endAccessor="end"
+        style={{ height: 570 }}
       />
     </div>
   )
 }
 
-export default MyCalendar;
-
-const events = [
-  {
-    'title': 'All Day Event very long title',
-    'allDay': true,
-    'start': new Date(2020, 8, 16),
-    'end': new Date(2020, 8, 16)
-  },
-  {
-    'title': 'Long Event',
-    'start': new Date(2020, 9, 15),
-    'end': new Date(2020, 8, 15)
-  },
-
-  {
-    'title': 'DTS STARTS',
-    'start': new Date(2016, 2, 13, 0, 0, 0),
-    'end': new Date(2016, 2, 20, 0, 0, 0)
-  },
-
-  {
-    'title': 'DTS ENDS',
-    'start': new Date(2016, 10, 6, 0, 0, 0),
-    'end': new Date(2016, 10, 13, 0, 0, 0)
-  },
-
-  {
-    'title': 'Some PARTY!!',
-    'start': new Date(2015, 3, 9, 0, 0, 0),
-    'end': new Date(2015, 3, 9, 0, 0, 0)
-  },
-  {
-    'title': 'Conference',
-    'start': new Date(2020, 8, 16),
-    'end': new Date(2020, 8, 16),
-    desc: 'Big conference for important people'
-  },
-  {
-    'title': 'YEAAAH!',
-    'start': new Date(2020, 6, 19, 7, 0, 0),
-    'end': new Date(2020, 6, 20, 10, 30, 0),
-    desc: 'Pre-meeting meeting, to prepare for the meeting'
-  },
-  ,
-  {
-    'title': 'Birthday Party',
-    'start': new Date(2020, 7, 20, 7, 0, 0),
-    'end': new Date(2020, 7, 20, 10, 30, 0),
-    desc: 'Pre-meeting meeting, to prepare for the meeting'
-  },
-  {
-    'title': 'Birthday Party 2',
-    'start': new Date(2020, 8, 20, 7, 0, 0),
-    'end': new Date(2020, 8, 20, 10, 30, 0)
-  },
-  {
-    'title': 'Birthday Party 3',
-    'start': new Date(2020, 8, 20, 7, 0, 0),
-    'end': new Date(2020, 8, 20, 10, 30, 0)
-  },
-];
+export default EventCalendar;

323920de43c6d21c575a995409f35ad8bdcbd1a0
Sander Koopmans <42515367+SanderKoopmans@users.noreply.github.com>
2020-08-26 14:57:56 +0200

    styling continued

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -6,17 +6,15 @@ import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import MaterialUiLink from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import DeveloperContext from '../contexts/DeveloperContext';
 import { Link, useHistory } from 'react-router-dom';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Snackbar from '@material-ui/core/Snackbar';
 import Alert from '@material-ui/lab/Alert';
+import DeveloperContext from '../contexts/DeveloperContext';
 
 const useStyles = makeStyles(theme => ({
   paper: {

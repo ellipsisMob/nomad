3d050eda4ba6e75a826e514dfaed2e310ffbca92
Kennet Timmers <kennettimmers@gmail.com>
2020-08-26 15:14:30 +0200

    email validation working

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -44,6 +44,10 @@ export default function SignUp() {
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
   const [loading, setLoading] = useState(false);
+  
+  const pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+
+  const isEnabled = pattern.test(email) && password.length > 0;
 
   let history = useHistory();
 
@@ -166,6 +170,7 @@ export default function SignUp() {
           </Grid> */}
         {/* </Grid> */}
         <Button
+          disabled={!isEnabled}
           type="submit"
           fullWidth
           variant="contained"
